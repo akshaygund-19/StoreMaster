ffc5830d85979d3f420d3ab18f1d6f8b4e38ce2b
akshaygund-19 <akshaygund07@outlook.com>
2025-07-18 16:38:42 +0530

    initial commit

---
+++ StoreMaster/src/main/java/com/akshay/StoreMaster/StoreMasterApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.StoreMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMasterApplication.class, args);
+	}
+
+}
