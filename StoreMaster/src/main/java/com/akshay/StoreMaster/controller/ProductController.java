d1d4d46a7d0f3a520cb45690b3bd74e87e58c869
akshaygund-19 <akshaygund07@outlook.com>
2025-07-26 14:35:57 +0530

    Added Delete and get All product endpoints

--- StoreMaster/src/main/java/com/akshay/StoreMaster/controller/ProductController.java
+++ StoreMaster/src/main/java/com/akshay/StoreMaster/controller/ProductController.java
@@ -2,12 +2,17 @@ package com.akshay.StoreMaster.controller;
 
 import com.akshay.StoreMaster.dto.ProductRequestDTO;
 import com.akshay.StoreMaster.dto.ProductResponseDTO;
+import com.akshay.StoreMaster.entity.Product;
 import com.akshay.StoreMaster.repository.ProductRepository;
 import com.akshay.StoreMaster.service.ProductService;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -19,17 +24,30 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
     //This endpoint handles adding a new product by accepting product details, delegating to the service layer, and returning the saved product information.
+    @Transactional
     @PostMapping("/add")
-    public ResponseEntity<ProductResponseDTO> add(@RequestBody ProductRequestDTO productRequestDTO){
-        var product = productService.addProduct(productRequestDTO);
-        log.info("Product added successful {}", productRequestDTO.getName());
-        return ResponseEntity.ok(product);
+    public ResponseEntity<List<ProductResponseDTO>> add(@RequestBody List<ProductRequestDTO> dtoList){
+        List<ProductResponseDTO> product = productService.addProduct(dtoList);
+        log.info("Product added successfully");
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
     @PutMapping("/update/{productId}")
     public ResponseEntity<ProductResponseDTO> update(@PathVariable int productId, @RequestBody ProductRequestDTO productRequestDTO){
         ProductResponseDTO updateProduct = productService.updateProduct(productId, productRequestDTO);
-        log.info("Product updated successfully Id:{}", productId);
+        log.info("Product updated successfully. Id:{}", productId);
         return ResponseEntity.ok(updateProduct);
+    }
+
+    @DeleteMapping("/delete/{productId}")
+    public ResponseEntity<String> delete(@PathVariable int productId){
+         productService.deleteProduct(productId);
+        log.info("Product deleted Successfully ID:{}",productId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/getAllProduct")
+    public List<ProductResponseDTO> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
--- StoreMaster/src/main/java/com/akshay/StoreMaster/service/ProductService.java
+++ StoreMaster/src/main/java/com/akshay/StoreMaster/service/ProductService.java
@@ -9,6 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -17,53 +22,65 @@ public class ProductService {
     private ProductRepository productRepository;
 
     //This method validates product input, saves the product to the database, and returns the saved product details as a response DTO
-    public ProductResponseDTO addProduct(ProductRequestDTO productRequestDTO) {
-        Product product = new Product();
-        if (productRequestDTO.getName() == null || productRequestDTO.getName().isBlank()) {
-            throw new IllegalArgumentException("Name cannot blank");
-        } else if (productRequestDTO.getPrice() < 0) {
-            throw new IllegalArgumentException("Price should be positive");
-        } else if (productRequestDTO.getStock_quantity() < 0) {
-            throw new IllegalArgumentException("Quantity should be positive");
-        } else {
-            product.setName(productRequestDTO.getName());
-            product.setPrice(productRequestDTO.getPrice());
-            product.setDescription(productRequestDTO.getDescription());
-            product.setStock_quantity(productRequestDTO.getStock_quantity());
-            product.setCategory(productRequestDTO.getCategory());
-            product.setCreated_at(LocalDateTime.now());
-            Product savedProduct = productRepository.save(product);
+    public List<ProductResponseDTO> addProduct(List<ProductRequestDTO> dtoList) {
+        List<ProductResponseDTO> listOfProductsToAdd = new ArrayList<>();
 
-            ProductResponseDTO responseDTO = new ProductResponseDTO();
-            responseDTO.setProduct_ID(savedProduct.getProduct_ID());
-            responseDTO.setName(savedProduct.getName());
-            responseDTO.setPrice(savedProduct.getPrice());
-            responseDTO.setDescription(savedProduct.getDescription());
-            responseDTO.setCategory(savedProduct.getCategory());
-            responseDTO.setStock_quantity(savedProduct.getStock_quantity());
-            return responseDTO;
+        for (ProductRequestDTO productRequestDTO : dtoList) {
+            if (productRequestDTO.getName() == null || productRequestDTO.getName().isBlank()) {
+                throw new IllegalArgumentException("Name cannot blank");
+            } else if (productRequestDTO.getPrice() < 0) {
+                throw new IllegalArgumentException("Price should be positive");
+            } else if (productRequestDTO.getStock_quantity() < 0) {
+                throw new IllegalArgumentException("Quantity should be positive");
+            }
+                Product product = new Product();
+                product.setName(productRequestDTO.getName());
+                product.setPrice(productRequestDTO.getPrice());
+                product.setDescription(productRequestDTO.getDescription());
+                product.setStock_quantity(productRequestDTO.getStock_quantity());
+                product.setCategory(productRequestDTO.getCategory());
+                product.setCreated_at(LocalDateTime.now());
+                Product savedProduct = productRepository.save(product);
+
+                ProductResponseDTO responseDTO = new ProductResponseDTO();
+                responseDTO.setProduct_ID(savedProduct.getProduct_ID());
+                responseDTO.setName(savedProduct.getName());
+                responseDTO.setPrice(savedProduct.getPrice());
+                responseDTO.setDescription(savedProduct.getDescription());
+                responseDTO.setCategory(savedProduct.getCategory());
+                responseDTO.setStock_quantity(savedProduct.getStock_quantity());
+                listOfProductsToAdd.add(responseDTO);
         }
+        return listOfProductsToAdd;
     }
 
     //This method updates an existing product’s details after validating input, saving changes to the database, and returning updated product information
     public ProductResponseDTO updateProduct(int productId, ProductRequestDTO productRequestDTO) {
-        Product checkProduct = productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("Product not found "));
-        if (productRequestDTO.getName() == null || productRequestDTO.getName().isBlank()) {
-            throw new IllegalArgumentException("Name cannot blank");
+        Optional<Product> checkProduct = productRepository.findById(productId);
+        if (checkProduct.isEmpty()) {
+            throw new IllegalArgumentException("Product not found ");
         }
-        if (productRequestDTO.getPrice() < 0) {
-            throw new IllegalArgumentException("Price should be positive");
-        }
-        if (productRequestDTO.getStock_quantity() < 0) {
-            throw new IllegalArgumentException("Quantity should be positive");
-        }
-        checkProduct.setName(productRequestDTO.getName());
-        checkProduct.setDescription(productRequestDTO.getDescription());
-        checkProduct.setPrice(productRequestDTO.getPrice());
-        checkProduct.setStock_quantity(productRequestDTO.getStock_quantity());
-        checkProduct.setCategory(productRequestDTO.getCategory());
 
-        Product savedProduct = productRepository.save(checkProduct);
+        Product product = checkProduct.get();
+
+        if (productRequestDTO.getName() != null || !productRequestDTO.getName().isBlank()) {
+            product.setName(productRequestDTO.getName());
+        }
+        if (productRequestDTO.getPrice() >= 0) {
+            product.setPrice(productRequestDTO.getPrice());
+        }
+        if (productRequestDTO.getDescription() != null) {
+            product.setDescription(productRequestDTO.getDescription());
+        }
+        if (productRequestDTO.getStock_quantity() >= 0) {
+            product.setStock_quantity(productRequestDTO.getStock_quantity());
+        }
+        if (productRequestDTO.getCategory() != null) {
+            product.setCategory(productRequestDTO.getCategory());
+        }
+        product.setCreated_at(LocalDateTime.now());
+
+        Product savedProduct = productRepository.save(product);
 
         ProductResponseDTO responseDTO = new ProductResponseDTO();
         responseDTO.setProduct_ID(savedProduct.getProduct_ID());
@@ -74,6 +91,29 @@ public class ProductService {
         responseDTO.setStock_quantity(savedProduct.getStock_quantity());
         return responseDTO;
     }
+
+    public void deleteProduct(int productId){
+        Optional<Product> checkProduct = productRepository.findById(productId);
+        if (checkProduct.isEmpty()){
+            throw new IllegalArgumentException("Product not found ");
+        }
+        checkProduct.ifPresent(productRepository::delete);
+    }
+
+    public List<ProductResponseDTO> getAllProducts(){
+        List<Product> allProducts = productRepository.findAll();
+
+        return allProducts.stream().map(product -> {
+
+            ProductResponseDTO responseDTO = new ProductResponseDTO();
+
+            responseDTO.setProduct_ID(product.getProduct_ID());
+            responseDTO.setPrice(product.getPrice());
+            responseDTO.setStock_quantity(product.getStock_quantity());
+            responseDTO.setName(product.getName());
+            responseDTO.setDescription(product.getDescription());
+            responseDTO.setStock_quantity(product.getStock_quantity());
+            return responseDTO;
+        }).collect(Collectors.toList());
+    }
 }
-
-
